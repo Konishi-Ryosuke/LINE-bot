abceda548bdf7a6d50626cc5b23486cc6b4fb274
Konishi-Ryosuke <61953480+Konishi-Ryosuke@users.noreply.github.com>
2020-08-07 16:58:05 +0900

    Add files via upload

--- Callback.java
+++ Callback.java
@@ -5,11 +5,9 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 
 @LineMessageHandler
 public class Callback {
@@ -19,51 +17,43 @@ public class Callback {
     private static final String USER_PASS = "b2180950";
     // フォローイベントに対応する
     @EventMapping
-    public List<PreExam> handleMessage(MessageEvent<TextMessageContent> event) throws SQLException {
+    public List<getCode> handleMessage(MessageEvent<TextMessageContent> event) throws SQLException {
         TextMessageContent tmc = event.getMessage();
         String text = tmc.getText();
         switch (text) {
-            case "日程を教えて":
-                return replyNittei(10);
-            default:
-                return reply(text);
+            case "月曜日の日程を教えて":
+                return replyNittei("月");
+            case "火曜日の日程を教えて":
+                return replyNittei("火");
+            case "水曜日の日程を教えて":
+                return replyNittei("水");
+            case "木曜日の日程を教えて":
+                return replyNittei("木");
+            case "金曜日の日程を教えて":
+                return replyNittei("金");
         }
+        return null;
     }
-
-
     //月～金の授業日程を返す
-    private List<PreExam> replyNittei(int lessThan) throws SQLException{
-        List<PreExam> list = new ArrayList<PreExam>();
-        String sql = "SELECT * FROM GAKUSEI_SCHEDULE";
+    private List<getCode> replyNittei(String week) throws SQLException{
+        List<getCode> list = new ArrayList<getCode>();
+        String sql = "SELECT * FROM GAKUSEI_SCHEDULE where WEEK = ?";
         try (Connection connection = DriverManager.getConnection(URL, USER_NAME, USER_PASS);
              PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, lessThan);
+            stmt.setString(1, week);
             ResultSet result = stmt.executeQuery();
             while (result.next()) {
                 String col1 = result.getString("WEEK");
                 String col2 = result.getString("STARTTIME");
                 String col3 = result.getString("SUBJECT");
-                PreExam preExam = new PreExam(col1,col2,col3);
+                getCode preExam = new getCode(col1,col2,col3);
                 list.add(preExam);
             }
         }
         return list;
     }
-
-
-    // 返答メッセージを作る
-    private TextMessage reply(String text) {
-        return new TextMessage(text);
-    }
-
-    // 画像メッセージを作る
-//    private ImageMessage replyImage(String url) {
-//        // 本来は、第一引数が実際の画像URL、第二画像がサムネイルのurl
-//        return new ImageMessage(url, url);
+//    // 返答メッセージを作る
+//    private TextMessage reply(String text) {
+//        return new TextMessage(text);
 //    }
-
-
-
-
-
 }
